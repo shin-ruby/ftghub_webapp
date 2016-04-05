f54e8ffc9bad043cf7e0a17bd669b58fdb926e90
shin-ruby <shin.php@gmail.com>
2016-04-05 14:06:55 +0800

    Finish user follow page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	end
+
+	def following
+		render 'show_follow'
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout' }
   root 'static_pages#home'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :fans
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
